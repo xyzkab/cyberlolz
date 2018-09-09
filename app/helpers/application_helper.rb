556dd7a777aed89b2697e7f52df56db571a8a73e
xyzkab <0xyzkab@gmail.com>
2018-09-09 19:50:54 +0700

    add :title and :full_title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def title(name)
+    provide(:title, name)
+  end
+
+  def full_title(title = '')
+    base_title = "Cyberlolz"
+    if title.empty?
+      base_title
+    else
+      "#{title} | #{base_title}"
+    end
+  end
 end
