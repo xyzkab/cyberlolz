5b8675c5a96a061f3538066d774d5fe76c4b4e4e
xyzkab <0xyzkab@gmail.com>
2018-09-09 19:51:27 +0700

    add :posts controller and views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  def show
+    
+  end
+end
