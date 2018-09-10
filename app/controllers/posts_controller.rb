fe906ecd20300cf03f8dab7dbdb804a1b9ad78f2
xyzkab <0xyzkab@gmail.com>
2018-09-10 15:39:05 +0700

    add :update,:edit,:show method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,31 @@ class PostsController < ApplicationController
   end
 
   def update
-    
+    @post = Post.find(params[:id])
+    if @post.update_attributes(update_params)
+      flash[:success] = "Post updated!"
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
     
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    @categories = Category.all
+    @status = PostStatus.all
+  end
+
   def show
-    
+    @post = Post.find(params[:id])
+  end
+  
+  private
+  def update_params
+    params.require(:post).permit(:description, :category_id, :post_status_id)
   end
 end
