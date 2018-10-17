3acbc5e45f8ca3e29cbc17c541d33a2f90f9794d
xyzkab <0xyzkab@gmail.com>
2018-10-17 17:17:10 +0700

    add tags crud

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,44 @@
+class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+  end
+
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(update_params)
+    
+    if @tag.save
+      flash[:success] = "tag added!"
+      redirect_to @tag
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update_attributes(update_params)
+      flash[:success] = "tag updated!"
+      redirect_to @tag
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def update_params
+    params.require(:tag).permit(:name, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
+  resources :tags
   resources :posts
   resources :categories
 end
