da14201a183d727a6a571145cb94da8173a75a77
xyzkab <0xyzkab@gmail.com>
2018-10-17 17:08:45 +0700

    add categories crud

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,44 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(update_params)
+    
+    if @category.save
+      flash[:success] = "category added!"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(update_params)
+      flash[:success] = "category updated!"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def update_params
+    params.require(:category).permit(:name, :description)
+  end
+end
