d4b9383ab3926bdbb16c3986fd9e9959162a6c8f
xyzkab <0xyzkab@gmail.com>
2019-06-14 00:48:45 +0700

    add :link_to_post method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,7 @@
 module PostsHelper
+  def link_to_post(post)
+    link_to(post.title[0..50], post_path(post))
+  end
   def title_to_link(post)
     link_to(post.title, post.url, target: :_blank )
   end
