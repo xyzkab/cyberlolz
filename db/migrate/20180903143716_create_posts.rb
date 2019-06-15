914dd4dbe2fe78d4e5a205dbf03765abff91ab40
xyzkab <0xyzkab@gmail.com>
2019-06-15 21:04:32 +0700

    change :description attribute from :string to :text

--- db/migrate/20180903143716_create_posts.rb
+++ db/migrate/20180903143716_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :description
+      t.text :description
       t.string :url
       t.string :url_uniq
 
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2018_09_06_143618) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "title"
-    t.string "description"
+    t.text "description"
     t.string "url"
     t.string "url_uniq"
     t.datetime "created_at", null: false
